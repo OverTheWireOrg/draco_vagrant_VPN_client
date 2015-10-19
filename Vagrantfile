116627cf4c2c5d439c93864502f47a584c175973
Bren Briggs <briggs.brenton@gmail.com>
2015-10-19 11:28:20 -0400

    Revert "Revert "Nonworking commit. Don't judge me.""
    This reverts commit 7c66c242a9f007ab091ba02b7767262993df756c.

--- Vagrantfile
+++ Vagrantfile
@@ -2,7 +2,17 @@
 # vi: set ft=ruby :
 
 Vagrant.configure(2) do |config|
-  config.vm.box = "ubuntu/trusty64"
-
-  config.vm.provision "shell", path: "bootstrap.sh"
+    config.vm.box = "ubuntu/trusty64"
+    config.vm.provision :shell do |shell|
+        shell.inline = "FACTER_LAN_SUBNET=$(ip route | sed -n '2p' | awk '{print $1}');
+                      export FACTER_LAN_SUBNET;
+                      mkdir -p /etc/puppet/modules;
+                      puppet module install puppetlabs-firewall"
+    end
+  config.vm.provision :puppet do |puppet|
+    puppet.manifests_path = "puppet/manifests"
+    puppet.module_path = "puppet/modules"
+    puppet.manifest_file = "site.pp"
+    puppet.options = "--verbose"
+  end
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,95 +1,77 @@
 #!/usr/bin/env bash
 
-# install openvpn client
-apt-get update
-apt-get -y install openvpn
+# Grab LAN subnet for Puppet to use when building firewall rules
+FACTER_LAN_SUBNET=$(ip route | sed -n '2p' | awk '{print $1}')
+export FACTER_LAN_SUBNET
 
-# add persistent iptables config
-cat > /etc/network/if-pre-up.d/iptablesload << EOL
-#!/bin/sh
-iptables-restore < /etc/iptables.rules
-exit 0
-EOL
-chmod +x /etc/network/if-pre-up.d/iptablesload
+# cat > /etc/iptables.rules << EOL
+# ########################################################################
+# # filter table
 
-cat > /etc/network/if-post-down.d/iptablessave << EOL
-#!/bin/sh
-iptables-save -c > /etc/iptables.rules
-exit 0
-EOL
-chmod +x /etc/network/if-post-down.d/iptablessave
+# # Drop anything we aren't explicitly allowing. All outbound traffic is okay
+# *filter
+# :INPUT   DROP
+# :FORWARD DROP
+# :OUTPUT  ACCEPT
 
-# add iptables rules
-LAN_SUBNET=$(ip route | sed -n '2p' | awk '{print $1}')
+# # chain for all input on eth0
+# :FW-eth0-INPUT -
+# # chain for all input on tun0
+# :FW-tun0-INPUT -
 
-cat > /etc/iptables.rules << EOL
-########################################################################
-# filter table
+# ########################################################################
+# # INPUT rules
 
-# Drop anything we aren't explicitly allowing. All outbound traffic is okay
-*filter
-:INPUT   DROP
-:FORWARD DROP
-:OUTPUT  ACCEPT
+# # Accept all on loopback interface
+# -A INPUT -i lo -j ACCEPT
 
-# chain for all input on eth0
-:FW-eth0-INPUT -
-# chain for all input on tun0
-:FW-tun0-INPUT -
+# # Accept ICMP packets needed for ping, traceroute, etc.
+# -A INPUT -p icmp --icmp-type echo-reply -j ACCEPT
+# -A INPUT -p icmp --icmp-type destination-unreachable -j ACCEPT
+# -A INPUT -p icmp --icmp-type time-exceeded -j ACCEPT
 
-########################################################################
-# INPUT rules
+# # Add custom INPUT filters
+# -A INPUT -i eth0 -j FW-eth0-INPUT
+# -A INPUT -i tun0 -j FW-tun0-INPUT
 
-# Accept all on loopback interface
--A INPUT -i lo -j ACCEPT
+# ########################################################################
+# # FW-eth0-INPUT rules
 
-# Accept ICMP packets needed for ping, traceroute, etc.
--A INPUT -p icmp --icmp-type echo-reply -j ACCEPT
--A INPUT -p icmp --icmp-type destination-unreachable -j ACCEPT
--A INPUT -p icmp --icmp-type time-exceeded -j ACCEPT
+# # Accept connections from LAN
+# -A FW-eth0-INPUT -s ${LAN_SUBNET} -j ACCEPT
 
-# Add custom INPUT filters
--A INPUT -i eth0 -j FW-eth0-INPUT
--A INPUT -i tun0 -j FW-tun0-INPUT
+# # Log anything on eth0 claiming it's from a local or non-routable network
+# # https://oav.net/mirrors/cidr.html
+# -A FW-eth0-INPUT -s 0.0.0.0/8          -j LOG --log-prefix "eth0-INPUT DROP LOCAL: "
+# -A FW-eth0-INPUT -s 10.0.0.0/8         -j LOG --log-prefix "eth0-INPUT DROP A: "
+# -A FW-eth0-INPUT -d 127.0.0.0/8        -j LOG --log-prefix "eth0-INPUT DROP LOOPBACK: "
+# -A FW-eth0-INPUT -s 169.254.0.0/16     -j LOG --log-prefix "eth0-INPUT DROP LINK-LOCAL: "
+# -A FW-eth0-INPUT -s 172.16.0.0/12      -j LOG --log-prefix "eth0-INPUT DROP B: "
+# -A FW-eth0-INPUT -s 192.168.0.0/16     -j LOG --log-prefix "eth0-INPUT DROP C: "
+# -A FW-eth0-INPUT -s 224.0.0.0/4        -j LOG --log-prefix "eth0-INPUT DROP MULTICAST D: "
+# -A FW-eth0-INPUT -s 240.0.0.0/5        -j LOG --log-prefix "eth0-INPUT DROP E: "
+# -A FW-eth0-INPUT -s 240.0.0.0/4        -j LOG --log-prefix "eth0-INPUT DROP FUTURE: "
+# -A FW-eth0-INPUT -s 248.0.0.0/5        -j LOG --log-prefix "eth0-INPUT DROP RESERVED: "
+# -A FW-eth0-INPUT -s 255.255.255.255/32 -j LOG --log-prefix "eth0-INPUT DROP BROADCAST: "
 
-########################################################################
-# FW-eth0-INPUT rules
+# # Accept any established connections
+# -A FW-eth0-INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-# Accept connections from LAN
--A FW-eth0-INPUT -s ${LAN_SUBNET} -j ACCEPT
+# # Log and drop everything else
+# -A FW-eth0-INPUT -j LOG --log-prefix "eth0-INPUT DROP: "
 
-# Log anything on eth0 claiming it's from a local or non-routable network
-# https://oav.net/mirrors/cidr.html
--A FW-eth0-INPUT -s 0.0.0.0/8          -j LOG --log-prefix "eth0-INPUT DROP LOCAL: "
--A FW-eth0-INPUT -s 10.0.0.0/8         -j LOG --log-prefix "eth0-INPUT DROP A: "
--A FW-eth0-INPUT -d 127.0.0.0/8        -j LOG --log-prefix "eth0-INPUT DROP LOOPBACK: "
--A FW-eth0-INPUT -s 169.254.0.0/16     -j LOG --log-prefix "eth0-INPUT DROP LINK-LOCAL: "
--A FW-eth0-INPUT -s 172.16.0.0/12      -j LOG --log-prefix "eth0-INPUT DROP B: "
--A FW-eth0-INPUT -s 192.168.0.0/16     -j LOG --log-prefix "eth0-INPUT DROP C: "
--A FW-eth0-INPUT -s 224.0.0.0/4        -j LOG --log-prefix "eth0-INPUT DROP MULTICAST D: "
--A FW-eth0-INPUT -s 240.0.0.0/5        -j LOG --log-prefix "eth0-INPUT DROP E: "
--A FW-eth0-INPUT -s 240.0.0.0/4        -j LOG --log-prefix "eth0-INPUT DROP FUTURE: "
--A FW-eth0-INPUT -s 248.0.0.0/5        -j LOG --log-prefix "eth0-INPUT DROP RESERVED: "
--A FW-eth0-INPUT -s 255.255.255.255/32 -j LOG --log-prefix "eth0-INPUT DROP BROADCAST: "
+# ########################################################################
+# # FW-tun0-INPUT rules
 
-# Accept any established connections
--A FW-eth0-INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+# # Accept any established connections
+# -A FW-tun0-INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-# Log and drop everything else
--A FW-eth0-INPUT -j LOG --log-prefix "eth0-INPUT DROP: "
+# # Log and drop anything else
+# -A FW-tun0-INPUT -j LOG --log-prefix "tun0-INPUT DROP: "
 
-########################################################################
-# FW-tun0-INPUT rules
+# COMMIT
+# EOL
 
-# Accept any established connections
--A FW-tun0-INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-
-# Log and drop anything else
--A FW-tun0-INPUT -j LOG --log-prefix "tun0-INPUT DROP: "
-
-COMMIT
-EOL
-
-# load iptables rules
-iptables-restore < /etc/iptables.rules
+# # load iptables rules
+# iptables-restore < /etc/iptables.rules
 
